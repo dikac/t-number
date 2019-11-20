d3b16efd5f7cde0d9aaea19a97a5747938efe23b
dikac <rigel234@yahoo.com>
2019-11-21 00:39:29 +0700

    add guard error

--- src/guard.ts
+++ src/guard.ts
@@ -1,11 +1,14 @@
 import Type from "./boolean/type";
 
-export default function Guard(value : any) : number {
+export default function Guard(
+    value : any,
+    error : (value: any) => Error = (value: any) => new Error('type expect string, given ' + value)
+) : number {
 
     if(Type(value)) {
 
         return value;
     }
 
-    throw new Error('type expect number, given ' + value);
+    throw error(value);
 }
