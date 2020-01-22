c8ef2f37edad0b28a1eca154d78818a86c4a6a32
dikac <rigel234@yahoo.com>
2020-01-22 17:23:08 +0700

    add precision to float-random

--- dist/random-float.d.ts
+++ dist/random-float.d.ts
@@ -1,3 +1,4 @@
 declare function RandomFloat(maximum: number): number;
 declare function RandomFloat(minimum: number, maximum: number): number;
+declare function RandomFloat(minimum: number, maximum: number, precision?: number): number;
 export default RandomFloat;
--- dist/random-float.js
+++ dist/random-float.js
@@ -9,16 +9,31 @@
 })(function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
-    function RandomFloat(minimum, maximum) {
+    function RandomFloat(minimum, maximum, precision) {
         if (maximum === undefined) {
             maximum = minimum;
             minimum = 0;
+        }
+        if (precision !== undefined && precision < 0) {
+            throw new Error(`precision must greater than 0`);
         }
         if (minimum > maximum) {
             throw new Error(`minimum(${minimum}) must not greater than maximum(${maximum})`);
         }
         let random = Math.random() * (maximum - minimum);
-        return random + minimum;
+        let result = random + minimum;
+        if (precision) {
+            let strings = result.toString().split('.');
+            if (strings[1] !== undefined) {
+                strings[1] = strings[1].substr(0, precision);
+                //console.log('===============');
+                //console.log(precision);
+                //console.log(strings);
+                result = parseFloat(strings.join('.'));
+                //console.log(result);
+            }
+        }
+        return result;
     }
     exports.default = RandomFloat;
 });
